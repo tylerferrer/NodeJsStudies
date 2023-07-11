385980ff2ee689809876a4a810b5d81ca13fe02c
tylerferrer <tylerlucasferrer@gmail.com>
2023-07-11 20:18:01 -0300

    chore: add jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.ts$':   'ts-jest'
   }
 };
 
---
+++ src/controller.spec.ts
@@ -0,0 +1,6 @@
+describe('', () => {
+    test('', () => {
+        expect(1).toBe(1)
+    })
+}
+)
